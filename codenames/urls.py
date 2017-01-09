12d8caa0ed2dc5a691ba4431f0d2139d6261cb14
dustin-chan-89 <busterdust@gmail.com>
2017-01-09 00:42:08 -0800

    works on localhost

--- codenames/urls.py
+++ codenames/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import codenames.views
+import app.views
 
 # Examples:
-# url(r'^$', 'codenames.views.home', name='home'),
+# url(r'^$', 'app.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', codenames.views.index, name='index'),
-    url(r'^db', codenames.views.db, name='db'),
+    url(r'^$', app.views.index, name='index'),
+    url(r'^db', app.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
