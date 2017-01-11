a657776d0e0676b8c098276133cbb71381b5c9be
dustin-chan-89 <busterdust@gmail.com>
2017-01-10 23:42:57 -0800

    remove

--- codenames/settings.py
+++ codenames/settings.py
@@ -48,7 +48,7 @@ SLACK_SCOPE = 'admin,bot,incoming-webhook,commands'
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
