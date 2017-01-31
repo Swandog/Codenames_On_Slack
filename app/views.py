bf8680c428661cb6b767194e3af8db8387135266
dustin-chan-89 <busterdust@gmail.com>
2017-01-30 18:46:38 -0800

    in channel message using response url

--- app/views.py
+++ app/views.py
@@ -258,7 +258,7 @@ def button(request):
     button_name = req_dict['actions'][0]['name']
     active_game_in_channel = Game.objects.get(channel_id=channel['id'])
 
-    requests.post(response_url, data=json.dumps({'text':'test', 'replace_original': False}))
+    requests.post(response_url, data=json.dumps({'text':'test', 'replace_original': False, "response_type": "in_channel"}))
 
     # detect if the user is picking a team
     if button_name == "blue" or button_name == "red":
