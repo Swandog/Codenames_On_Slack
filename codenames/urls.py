386d7ffd34471fea41c3871598a1abf129748e73
Dustin Chan <dustin.c.chan@gmail.com>
2019-08-22 09:00:21 -0700

    Update urls.py
    fix urls for django upgrade

--- codenames/urls.py
+++ codenames/urls.py
@@ -10,11 +10,11 @@ import app.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', app.views.index),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', app.views.index, name='index'),
+    url(r'^admin/', admin.site.urls),
     url(r'^generate_wordset$', app.views.generate_wordset),
     url(r'^initialize_game$', app.views.initialize_game),
-    url(r'^slack/', include('django_slack_oauth.urls')),
+    url(r'^slack/', 'django_slack_oauth.urls'),
     url(r'^button/', app.views.button),
     url(r'^close_teams', app.views.close_teams),
     url(r'^test_webhook', app.views.test_webhook),
